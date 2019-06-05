a83c03401e0a48c78d84341618ef0b065ac5c79f
Bhatia, Binit <47129292+bhatibi@users.noreply.github.com>
2019-06-04 21:48:12 -0400

    Add POST end-point for adding todo item

--- app.js
+++ app.js
@@ -16,6 +16,34 @@ app.get('/api/v1/todos', (req, res) => {
     });
 });
 
+// POST request
+app.post('/api/v1/todos', (req, res) => {
+    if (!req.body.title) {
+        return res.status(400).send({
+            success: 'false',
+            message: 'title is required'
+        });
+    } else if(!req.body.description) {
+        return res.status(400).send({
+            success: 'false',
+            message: 'description is required'
+        });
+    }
+
+    const todo = {
+        id: db.length + 1,
+        title: req.body.title,
+        description: req.body.description
+    };
+
+    db.push(todo);
+    return res.status(201).send({
+        success: 'true',
+        message: 'todo added successfully',
+        todo
+    });
+});
+
 const PORT = 5000;
 
 app.listen(PORT, () => {
